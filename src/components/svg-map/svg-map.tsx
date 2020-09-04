5b8e61f5d54ccc2f5c2df730c007a6b8ef9f83f2
Robert Young <robertyoung10101@gmail.com>
2020-09-04 19:50:28 +1200

    Map svg can be dragged and resized

--- src/components/svg-map/svg-map.tsx
+++ src/components/svg-map/svg-map.tsx
@@ -2,11 +2,19 @@ import React from 'react';
 import { UncontrolledReactSVGPanZoom } from 'react-svg-pan-zoom';
 import { ReactSvgPanZoomLoader } from 'react-svg-pan-zoom-loader';
 const SvgMap = () => {
+    const pageWidth = window.innerWidth;
+    const pageHeight = window.innerHeight;
     return (
         <div>
             <ReactSvgPanZoomLoader src="genabackis-map.svg" render={content => (
-                <UncontrolledReactSVGPanZoom width={500} height={500}>
-                    <svg width={500} height={500}>
+                <UncontrolledReactSVGPanZoom
+                    width={pageWidth}
+                    height={pageHeight}
+                    tool="pan"
+                    background="#FFFFFF"
+                    toolbarProps={{position: 'none'}}
+                    miniatureProps={{position: 'none', background: '#FFFFFF', width: 0, height: 0}}>
+                    <svg width={pageWidth} height={pageHeight}>
                         {content}
                     </svg>  
                 </UncontrolledReactSVGPanZoom>
