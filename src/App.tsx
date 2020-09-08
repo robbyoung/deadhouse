ee9f4337c96bb1e5b658e08c3ef743696ddb2100
Robert Young <robertyoung10101@gmail.com>
2020-09-09 09:38:00 +1200

    Show and hide svg layers based on filter contents

--- src/App.tsx
+++ src/App.tsx
@@ -2,26 +2,39 @@ import React, {useState} from 'react';
 import SvgMap from './components/svg-map';
 import MultiSelect, {MultiSelectOption} from './components/multi-select';
 import './App.css';
+import genabackisOptions from './maps/genabackis';
+
+function showHideMapLayers(
+    selection: MultiSelectOption[],
+    available: MultiSelectOption[],
+): void {
+    available.forEach((layer) => {
+        const element = document.getElementById(layer.value);
+        const isSelected = selection.find(
+            (option) => option.value === layer.value,
+        );
+
+        if (element) {
+            if (isSelected) {
+                element.classList.remove('hidden');
+            } else {
+                element.classList.add('hidden');
+            }
+        }
+    });
+}
 
 function App(): JSX.Element {
-    const mapOptions: MultiSelectOption[] = [
-        {
-            label: 'Outline',
-            value: 'Outline',
-        },
-        {
-            label: 'Cities',
-            value: 'Cities',
-        },
-    ];
-    const [selectedOptions, setMapOptions] = useState(mapOptions);
+    const [selectedOptions, setMapOptions] = useState(genabackisOptions);
+
+    showHideMapLayers(selectedOptions, genabackisOptions);
 
     return (
         <div className="App">
             <div className="filters">
                 <MultiSelect
                     title="Map Features"
-                    options={mapOptions}
+                    options={genabackisOptions}
                     selected={selectedOptions}
                     onSelectionChange={(selection): void =>
                         setMapOptions(selection)
---
+++ src/maps/genabackis.tsx
@@ -0,0 +1,18 @@
+import {MultiSelectOption} from '../components/multi-select';
+
+const genabackisOptions: MultiSelectOption[] = [
+    {
+        label: 'Outline',
+        value: 'layer2',
+    },
+    {
+        label: 'Cities',
+        value: 'layer3',
+    },
+    {
+        label: 'Rivers and Lakes',
+        value: 'layer4',
+    },
+];
+
+export default genabackisOptions;
