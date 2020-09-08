510fb54f0a2cd74df75777645989dbb342a17e17
Robert Young <robertyoung10101@gmail.com>
2020-09-08 16:02:05 +1200

    Added props and tests for multi select component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
+import React, {useState} from 'react';
 import SvgMap from './components/svg-map';
-import MultiSelect from './components/multi-select';
+import MultiSelect, {MultiSelectOption} from './components/multi-select';
 import './App.css';
 
 function App(): JSX.Element {
-    const mapOptions = [
+    const mapOptions: MultiSelectOption[] = [
         {
             label: 'Outline',
             value: 'Outline',
@@ -14,11 +14,19 @@ function App(): JSX.Element {
             value: 'Cities',
         },
     ];
+    const [selectedOptions, setMapOptions] = useState(mapOptions);
 
     return (
         <div className="App">
             <div className="filters">
-                <MultiSelect title="Map Features" options={mapOptions} />
+                <MultiSelect
+                    title="Map Features"
+                    options={mapOptions}
+                    selected={selectedOptions}
+                    onSelectionChange={(selection): void =>
+                        setMapOptions(selection)
+                    }
+                />
             </div>
             <div className="map">
                 <SvgMap src="genabackis-map.svg" />
--- src/components/multi-select.tsx
+++ src/components/multi-select.tsx
@@ -2,17 +2,33 @@ import React from 'react';
 import Select from 'react-select';
 import './multi-select.css';
 
+export interface MultiSelectOption {
+    value: string;
+    label: string;
+}
+
 interface MultiSelectProps {
     title: string;
-    options: {value: string; label: string}[];
+    options: MultiSelectOption[];
+    selected: MultiSelectOption[];
+    onSelectionChange: (options: MultiSelectOption[]) => void;
 }
 
 const MultiSelect = (props: MultiSelectProps): JSX.Element => {
-    const {title, options} = props;
+    const {title, options, selected, onSelectionChange} = props;
     return (
         <div className="multi-select">
             <p className="title">{title}</p>
-            <Select isMulti options={options} />
+            <Select
+                isMulti
+                options={options}
+                onChange={(selection): void =>
+                    onSelectionChange(
+                        selection ? (selection as MultiSelectOption[]) : [],
+                    )
+                }
+                value={selected}
+            />
         </div>
     );
 };
