511ca71e5891da654ba8f46dc035a4256aa31270
Robert Young <robertyoung10101@gmail.com>
2021-02-16 20:49:20 +1300

    Added a mock paths item in the sidebar

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import {FaMap, FaWifi} from 'react-icons/fa';
+import {FaMap} from 'react-icons/fa';
+import {GiFootsteps} from 'react-icons/gi';
 import {Menu, MenuItem, ProSidebar, SubMenu} from 'react-pro-sidebar';
 import 'react-pro-sidebar/dist/css/styles.css';
 import {Link} from 'react-router-dom';
@@ -8,8 +9,8 @@ import './sidebar.css';
 function Sidebar(): JSX.Element {
     return (
         <ProSidebar collapsed>
-            <Menu iconShape="square">
-                <SubMenu icon={<FaMap />} title="Maps">
+            <Menu>
+                <SubMenu icon={<FaMap size={22} />} title="Maps">
                     <MenuItem>
                         <Link to="/genabackis">Genabackis</Link>
                     </MenuItem>
@@ -19,6 +20,17 @@ function Sidebar(): JSX.Element {
                     <MenuItem>
                         <Link to="/quon-tali">Quon Tali</Link>
                     </MenuItem>
+                </SubMenu>
+                <SubMenu icon={<GiFootsteps size={40} />} title="Paths">
+                    <SubMenu title="Gardens of the Moon">
+                        <MenuItem>Ganoes Paran</MenuItem>
+                    </SubMenu>
+                    <SubMenu title="Deadhouse Gates" popperarrow>
+                        <MenuItem>Icarium & Mappo</MenuItem>
+                    </SubMenu>
+                    <SubMenu title="Memories of Ice">
+                        <MenuItem>Gruntle & Stonny</MenuItem>
+                    </SubMenu>
                 </SubMenu>
             </Menu>
         </ProSidebar>
