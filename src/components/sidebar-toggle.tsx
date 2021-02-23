75f587048d5d580160eb085f2fbc8ea8b1855a54
Robert Young <robertyoung10101@gmail.com>
2021-02-23 19:58:21 +1300

    Added styles to the sidebar toggle

--- src/components/sidebar-toggle.tsx
+++ src/components/sidebar-toggle.tsx
@@ -12,9 +12,13 @@ function SidebarToggle(props: SidebarToggleProps): JSX.Element {
     const {toggle, onToggle} = props;
     return (
         <div>
-            <button type="button" onClick={() => onToggle(toggle.value)}>
-                {toggle.toggledOn ? <FaCheckSquare /> : <FaSquare />}
-                {toggle.label}
+            <button type="button" onClick={(): void => onToggle(toggle.value)}>
+                {toggle.toggledOn ? (
+                    <FaCheckSquare className="checkbox" />
+                ) : (
+                    <FaSquare className="checkbox" />
+                )}
+                <p>{toggle.label}</p>
             </button>
         </div>
     );
